220dab3ec241d1e333d824b07f08026a9f745d6e
Viraj Jamdhade <viraj.jamdhade24@pcu.edu.in>
2025-06-04 17:01:33 +0530

    Create Determine no. is +VE or -VE.cpp

---
+++ Determine no. is +VE or -VE.cpp
@@ -0,0 +1,22 @@
+//  POSITIVE NEGATIVE NUMBER
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int n ;
+    cout <<"Enter the number : ";
+    cin>> n;
+
+    if( n>= 0)
+    {
+        cout << "Number is a Positive number";
+    }
+    else
+    {
+        cout<<"Number is a Negative number";
+    }
+
+    return 0;
+}
